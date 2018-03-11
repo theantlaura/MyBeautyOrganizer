d71bd53337217dfa5e29e554382eeb0c0c252303
cobiamalu <cobiamalu@ggc.edu>
2018-03-11 14:39:26 -0400

    Removed the style from the header and added it to the css, this is the modified version. I will keep changing it though

--- MyBeautyOrganizer/_/components/php/header.php
+++ MyBeautyOrganizer/_/components/php/header.php
@@ -1,35 +1,16 @@
 <header class="clearfix" style="focus: background-color: none;">
 	<section id="branding" >
 		<a href="index.php"><img src="images/misc/prelogo.png" style = width:150px;height:100px;alt="Logo for My Beauty Organizer"></a> 
-		         <meta name="viewport" content="width=device-width, initial-scale=10">
-                  <div style="text-align: right;">
-                  <style> 
-                  input[type=text] { 
-                  width: 130px;
-                  box-sizing: border-box;
-                  border: 2px solid #ccc;
-                  border-radius: 4px;
-                  font-size: 16px;
-                  background-color: white;
-                  background-position: 10px 10px; 
-                  background-repeat: no-repeat;
-                  padding: 2px 2px 2px 4px;
-                  -webkit-transition: width 0.4s ease-in-out;
-                  transition: width 0.4s ease-in-out;
-              }
-
-              input[type=text]:focus {
-                  width: 100%;
-              }
-              </style>
-              </div>
              <body>
                <div class="search-container">    
    				  <form action="/action_page.php">
+                    <div style="text-align: right;">
     		        <input type="text" placeholder="Search.." name="search">
-     			     <button type="submit">Submit</button>
-  			      </form>
+     			     <button type="submit"> Go </button>
+                     </div>
+                  </form>
   				</div>
+                
     </section><!-- branding -->
 
 	<section class="navbar">  <!--style=" background-color: #ffced6"-->
