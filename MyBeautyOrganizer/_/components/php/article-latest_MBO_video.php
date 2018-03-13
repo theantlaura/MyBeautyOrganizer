8b90f82dffcb2ac56c55f9886a645afc40e58ad5
theantlaura <larevalo@ggc.edu>
2018-03-13 03:48:41 -0400

    added content to home page, revised search bar

---
+++ MyBeautyOrganizer/_/components/php/article-latest_MBO_video.php
@@ -0,0 +1,12 @@
+<article>
+	<h2>MBO Reviews the Kat Von D Shade + Light Glimmer Palette</h2>
+	<div class="media-body">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/5-v57NV1cu0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+		<p>Here is the MBO review of the Kat Vod D Shade + Light Glimmer eyeshadow palette released in July 2017.
+        This is a limited edition palette meant as an accompanient to the original Shade + Light eyeshadow
+        palette.  I fell in love with it after I discovered the shimmer shades make excellent highlighters.  Watch for
+        the full review.  Swatches are on primed skin and with a brush - just like eyeshadow would be applied to the lid.</p>
+		
+	</div>
+</article>
+
---
+++ MyBeautyOrganizer/_/components/php/article-latest_beauty_news.php
@@ -0,0 +1,15 @@
+<article class="latestnews media">
+	<h2>Latest Beauty News</h2>
+	<div class="media-body">
+    <h3>Nars Narsissist Wanted Cheek Palettes Launch March 08, 2018</h3>
+		<a class="pull-right"><img src="images/makeup/highend/nars/blush/Nars_Narsissist_Wanted_Cheek_Palette_Vol1_My_Beauty_Organizer.jpg" 
+        alt="Photo of Nars Narsissist Wanted Cheek Palette Volume One New Makeup for Spring 2018"></a>
+        <a class="pull-right"><img src="images/makeup/highend/nars/blush/Nars_Narsissist_Wanted_Cheek_Palette_Vol2_My_Beauty_Organizer.jpg" 
+        alt="Photo of Nars Narsissist Wanted Cheek Palette Volume Two New Makeup for Spring 2018"></a>
+		<p>High-profile shades. Made for play. Take your status to the next level with limited-edition 
+        NARSissist Wanted Cheek Palettes. Swipe and sculpt your way to customized cheeks with a coveted 
+        wardrobe of matte and shimmer blushes in the cult-favorite NARS formula. Brighten up with cool 
+        corals and pops of pink, or go bold with radiant reds and deep raspberry. </p>
+		
+	</div>
+</article>
