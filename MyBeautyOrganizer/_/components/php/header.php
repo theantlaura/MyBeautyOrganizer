46df0c1da8222135b24e733df51d00f97674cf79
cobiamalu <cobiamalu@ggc.edu>
2018-03-26 14:08:26 -0400

    Fixed the search bar

--- MyBeautyOrganizer/_/components/php/header.php
+++ MyBeautyOrganizer/_/components/php/header.php
@@ -59,11 +59,57 @@ border: 3px solid #FFC0CB;
 				  <li><a tabindex="-1" href="community.php#Beauty_Advice">Beauty Advice</a></li>
 				</ul><!-- dropdown menu -->
 			<li><a href="reviews.php">Reviews</a></li>
-			<li><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <form class="example" action="action_page.php">
-  			<input type="text" placeholder="Search.." name="search">
- 			<button type="submit"><i class="fa fa-search"></i></button>
-			</form></li>
+			<li>
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+              <style>
+              body {
+                  font-family: Arial;
+              }
+
+              * {
+                  box-sizing: border-box;
+              }
+
+              form.example input[type=text] {
+                  padding: 10px;
+                  font-size: 17px;
+                  border: 1px solid grey;
+                  float: left;
+                  width: 80%;
+                  background: #f2e3e3;
+              }
+
+              form.example button {
+                  float: left;
+                  width: 20%;
+                  padding: 10px;
+                  background: #fce0f0;
+                  color: white;
+                  font-size: 17px;
+                  border: 1px solid grey;
+                  border-left: none;
+                  cursor: pointer;
+              }
+
+              form.example button:hover {
+                  background: #f998ca;
+              }
+
+              form.example::after {
+                  content: "";
+                  clear: both;
+                  display: table;
+              }
+              </style>
+              <body>
+			<form class="example" action="/action_page.php" style="margin:auto;max-width:300px">
+		  	<input type="text" placeholder="Search.." name="search2">
+  			<button type="submit"><i class="fa fa-search"></i></button>
+			</form>
+
+              </body>
+              </li>
         
         <!-- nav -->
 	</section><!-- navbar -->
