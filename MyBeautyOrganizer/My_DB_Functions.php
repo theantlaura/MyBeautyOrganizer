519119a67dcaadead91e95a46669191f9219214e
theantlaura <larevalo@ggc.edu>
2018-03-26 17:10:20 -0400

    added My_DB_Functions file

---
+++ MyBeautyOrganizer/My_DB_Functions.php
@@ -0,0 +1,54 @@
+<?php
+function My_Connect_DB()
+{
+$servername = "localhost";
+$username = "mybeautyorganizer";
+$password = "";
+$dbname = "my_mybeautyorganizer";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if(!$conn)
+die("Connection to DB failed: ".mysqli_connect_error(). "<br/>");
+return $conn;
+}
+function My_SQL_EXE($conn,$sql){
+if($result = mysqli_query($conn, $sql))
+echo "SQL is done successfully<br/>";
+else
+echo "Error in running sql:  ".$sql ."with error: ".mysqli_error($conn)."<br/>";
+return $result;
+}
+
+function My_Show_Table($result){
+echo "<table border = 1";
+echo "<tr>";
+while($fieldinfo = mysqli_fetch_field($result))
+{
+echo "<td>";
+echo $fieldinfo->name;
+echo "</td>";
+}
+echo "</tr>";
+while($row = mysqli_fetch_assoc($result))
+{
+echo "<tr>";
+foreach($row as $key=>$value)
+{
+echo "<td>";
+echo $value;
+echo "</td>";
+}
+echo "</tr>";
+}
+echo "</table>";
+echo "Total rows: ".mysqli_num_rows($result)."<br/>";
+}
+
+function Run_SQL_Show_Table($conn, $sql, $table)
+{
+My_SQL_EXE($conn, $sql);
+$sql = "SELECT * FROM ". $table.";";
+$result = My_SQL_EXE($conn, $sql);
+My_Show_Table($result);
+}
+?>
