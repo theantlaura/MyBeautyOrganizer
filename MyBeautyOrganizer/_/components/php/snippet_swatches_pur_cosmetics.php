02eee911d2f8be75fa1fea24d63a9535bce5c9c5
theantlaura <larevalo@ggc.edu>
2018-04-02 00:21:25 -0400

    started swatches page, add images, created snippet for swatches

---
+++ MyBeautyOrganizer/_/components/php/snippet_swatches_pur_cosmetics.php
@@ -0,0 +1,38 @@
+<div class="tab-pane active" id="pur_cosmetics">
+	<h2>Pur Cosmetics</h2>
+	<p>Pur Cosmetics has been uping their game lately with these recent releases below!  A recent
+    collab with Boxy Charm is a giant step in the right direction.</p>
+
+	<section class="media">
+		<a class="pull-left" data-toggle="tooltip" title data-original-title=
+        "pur_cosmetics"><img class="img-rounded" src="images/swatches/Pur_Cosmetics_Boxy_Charm_Eyeshadow_Palette_Swatches_My_Beauty_Organizer.jpg" 
+        alt="Photo of swatches from the Pur Cosmetics Boxy Charm Eyeshadow Palette by My Beauty Organizer website."></a>
+
+	  <div class="media-body">
+	  	<h4>Boxy Charm Eyeshadow Palette</h4>
+			<a href = "https://www.purcosmetics.com/pur-x-boxycharm-eyeshadow-palette">Buy Boxy Charm</a>
+			<p>Versatile, pigmented and transformative—from lightweight, warm matte shades of peach to a
+            vibrate blue or shimmery pink, this Boxycharm eyeshadow palette is designed to help you achieve 
+            a fun look that’s sure to make your eyes pop. Expect silky bendability and pure color intensity
+            in 12 highly pigmented, blendable shades that can be worn alone or in any combination for endless 
+            eye looks.</p>
+	  </div><!-- media-body -->
+	</section><!-- media -->
+
+	<section class="media">
+		<a class="pull-left" data-toggle="tooltip" title data-original-title=
+        "pur_cosmetics"><img class="pull-left img-rounded" src="images/swatches/" alt="Photo of swatches 
+        from the Pur Cosmetics Pro X Etienne Ortega Eyeshadow Palette by My Beauty Organizer website."></a>
+	  
+      <div class="media-body">
+	  	<h4>Pro X Etienne Ortega Eyeshadow Palette</h4>
+			<a href = "https://www.purcosmetics.com/pur-pro-eyeshadow-palette-etienne-ortega">Buy Pro X</a>
+			<p>What it is: Etienne partnered with PÜR to curate a PÜR PRO eye shadow collection that evokes
+            glamourous looks for global skin tones. This must-have palette is sure to entice the novice and 
+            the pro.</p>
+	  </div><!-- media-body -->
+	</section><!-- media -->
+
+	
+</div><!-- pur cosmetics -->
+
