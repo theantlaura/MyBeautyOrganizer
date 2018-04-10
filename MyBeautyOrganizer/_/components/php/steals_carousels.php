595840ef9f2ee01104eac7f2792c3a9b3a727bf9
cobiamalu <cobiamalu@ggc.edu>
2018-04-10 02:45:10 -0400

    Added some carousels for steals n deals

--- MyBeautyOrganizer/_/components/php/steals_carousels.php
+++ MyBeautyOrganizer/_/components/php/steals_carousels.php
@@ -9,8 +9,8 @@
 	</ol><!-- carousel-indicators -->
 
 	<section class="carousel-inner">
-		<div class="active item"><img src="images/carousel/Anastasia_ Beverly_Hills_Soft_Glam_Eyeshadow_Palette_My_Beauty_Organizer.jpg" alt="Photo of Anastasia Beverly Hills Soft Glam Eyeshadow Palette March 2018"></div>
-		<div class="item"><img src="images/carousel/Kat_Von_D_Divine_Eyeshadow_Palette_My_Beauty_Organizer.jpg" alt="Photo of the Kat Von D Divine Eyeshadow Palette January 2018 "></div>
+		<div class="active item"><img src="images/carousel/stealsmain.png" alt="First photo for steals n' deals"></div>
+		<div class="item"><a href="https://www.ulta.com/promotion/buy-more-save-more/"><img src="images/carousel/Ulta1.png" alt="Ulta's steals"style="width:400px;height:200px;border:0"><img src="images/carousel/Ulta2.png" alt="Ulta's steals 2" style="width:400px;height:200px;border:0"></a></div>
 		<div class="item"><img src="images/carousel/Huda_Mini_Eyeshadow_Palettes_My_Beauty_Organizer.jpg" alt="Photo of Huda Mini Eyeshadow Palettes for My Beauty Organizer"></div>
 		<div class="item"><img src="images/carousel/Natasha_Denona_Tropic_Eyeshadow_Palette_My_Beauty_Organizer.jpg" alt="Photo of the Natasha Denona Tropic Eyeshadow Palette March 2018 New Spring Makeup"></div>
 		<div class="item"><img src="images/carousel/Dior_Spring_Nailpolish_My_Beauty_Organizer.jpg" alt="Photo of Dior Spring 2018 Nailpolish for My Beauty Organizer"></div>
@@ -18,4 +18,4 @@
 
 	<a href="#myCarousel" class="left carousel-control" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
 	<a href="#myCarousel" class="right carousel-control" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-</div><!-- myCarousel -->
+</div><!-- myCarousel -->
